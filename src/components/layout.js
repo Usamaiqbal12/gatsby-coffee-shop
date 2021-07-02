c9c1ef9499d286785aac51c3f3b862efbd7f862f
Usamaiqbal12 <musama.iqbal1998@gmail.comm>
2021-07-02 04:55:16 -0700

    first  commit

---
+++ src/components/layout.js
@@ -0,0 +1,25 @@
+
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import './bootstrap.min.css'
+
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  
+  return (
+    <>
+    
+        <main>{children}</main>
+       
+  
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
